faee3ee86a9de321b5bc407b5480d09264279710
Michelle Ferreirae <mnfmnfm@gmail.com>
2024-01-16 10:30:00 -0500

    initial commit; wrote BakedGood class constructor

---
+++ BakedGood.java
@@ -0,0 +1,25 @@
+
+/**
+ * Represents a single baked good in our bakery's inventory
+ *
+ */
+public class BakedGood
+{
+    // instance variables
+    private String name;
+    private String recipe;
+    private double price;
+    private int quantity;
+
+    /**
+     * Constructor for objects of class BakedGood
+     */
+    public BakedGood(String name, String recipe, double price, int quantity)
+    {
+        // initialise instance variables
+        this.name = name;
+        this.recipe = recipe;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
